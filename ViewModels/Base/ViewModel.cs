48df26ab5eb0c6974c4c41e897481f69ce07afde
Леонид <Lenya.g.d@mail.ru>
2021-01-26 22:03:11 +0300

    Базовый класс ViewModel

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Helper_postgreSQL.ViewModels.Base
+{
+    internal abstract class ViewModel: INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+   
+        protected virtual void OnPropertyChanget([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PeopsertName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanget(PeopsertName);
+            return true;
+        }
+    }
+}
