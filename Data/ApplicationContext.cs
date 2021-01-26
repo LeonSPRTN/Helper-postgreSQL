4e6664fcda861df0a83231680764d45245c02747
Леонид <Lenya.g.d@mail.ru>
2021-01-26 20:39:15 +0300

    Добавьте файлы проекта.

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Helper_postgreSQL.Models;
+using System.Data.Entity;
+
+namespace Helper_postgreSQL.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Setting> Settings { get; set; }
+        public ApplicationContext() : base("DefaultConnection")
+        {
+        }      
+    }
+}
---
+++ Models/Setting.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Helper_postgreSQL.Models
+{
+    public class Setting : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string path_bin;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Path_bin
+        {
+            get { return path_bin; }
+            set { path_bin = value; }
+        }
+    }
+}
